e9c725335172dcebcbe036e3774aa27f453f845d
Beeriand <adiche@wp.pl>
2020-04-01 17:33:48 +0200

    CarMenu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,32 +8,54 @@ public class Menu : MonoBehaviour
 
     public Canvas can;
     public Canvas HUD;
+    public Canvas carMenue;
     public Button btnStart;
+    public Button btnPlay;
+    public Button btnPlay2;
+    public Button btnPlay3;
+    public Button btnBack;
     public Button btnQuit;
 
     void Start()
     {
         can = can.GetComponent<Canvas>();
         HUD = HUD.GetComponent<Canvas>();
+        carMenue = carMenue.GetComponent<Canvas>();
         btnStart = btnStart.GetComponent<Button>();
+        btnPlay = btnPlay.GetComponent<Button>();
+        btnPlay2 = btnPlay2.GetComponent<Button>();
+        btnPlay3 = btnPlay3.GetComponent<Button>();
+        btnBack = btnBack.GetComponent<Button>();
         btnQuit = btnQuit.GetComponent<Button>();
         HUD.enabled = false;
+        carMenue.enabled = false;
         Time.timeScale = 0;
     }
 
 
     public void BtnStart()
     {
+        carMenue.enabled = true;
         can.enabled = false;
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        Time.timeScale = 1;
-        HUD.enabled = true;
-        AudioSource[] aSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
-        foreach (AudioSource source in aSources)
-        {
-            source.Play();
-        }
+    }
+
+    public void BtnPlay()
+    {
+
+    } 
+
+    public void BtnPlay2()
+    {
+
+    } 
+
+    public void BtnPlay3()
+    {
+
+    }
+    public void BtnBack()
+    {
+
     }
 
     public void BtnQuit()
